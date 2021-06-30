fd2afeceac6801614079489cd95983d2943f4b5e
elegs99 <71243839+elegs99@users.noreply.github.com>
2021-06-29 21:30:01 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -5,29 +5,47 @@ $(document).ready(function(){
     $('.project1').hide();
     $('.project2').hide();
     // add header background when scrolling
+    // add scroll up botton when scrolling
     $(window).scroll(function(){
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
+
     // Toggle menu button
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
-    // opens and closes popup
+
+    // Typing animation
+    var typed = new Typed(".typing", {
+        strings: ["a Student", "a Programmer", " your Next Hire"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // Opens and closes popup
     $('.grid-item1').click(function(){
         $('.overlay').show()
         $('.project1').show()
         $('.popup-content').show()
+        $('.scroll-up-btn').removeClass("show");
         $('body').css('overflow','hidden')
     });
     $('.grid-item2').click(function(){
         $('.overlay').show()
         $('.project2').show()
         $('.popup-content').show()
+        $('.scroll-up-btn').removeClass("show");
         $('body').css('overflow','hidden')
     });
     $('.close-btn').click(function(){
